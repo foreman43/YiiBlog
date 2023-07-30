7d6fcde61c393bd6554bb09f3da0536776c76914
Артём Лукьянов <a-lukyanov@bk.ru>
2023-07-30 15:17:55 +0400

    removed the id field from the form

--- views/Post/_search.php
+++ views/Post/_search.php
@@ -15,17 +15,11 @@ use yii\widgets\ActiveForm;
         'method' => 'get',
     ]); ?>
 
-    <?= $form->field($model, 'id') ?>
-
     <?= $form->field($model, 'title') ?>
-
-    <?= $form->field($model, 'content') ?>
 
     <?= $form->field($model, 'status') ?>
 
     <?= $form->field($model, 'created_at') ?>
-
-    <?php // echo $form->field($model, 'updated_at') ?>
 
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
