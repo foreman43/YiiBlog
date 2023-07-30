bdc4185133c59b7efc4f7584f17fcab108f32d41
Артём Лукьянов <a-lukyanov@bk.ru>
2023-07-30 15:11:41 +0400

    create button now displayed only if us is logged in

--- views/Post/index.php
+++ views/Post/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= !Yii::$app->user->isGuest ? Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) : '' ?>
     </p>
 
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
